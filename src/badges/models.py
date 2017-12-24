d82c0d901f460878b3952177d04d37f45413d79e
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-12-24 19:19:56 +0100

    create badge models

--- src/badges/models.py
+++ src/badges/models.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Star(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Collector(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Pioneer(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
