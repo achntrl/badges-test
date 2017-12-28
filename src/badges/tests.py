c554566d563a6cd2df3a165aee869edbab4ce4a0
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-12-28 14:12:03 +0100

    test that badges arent given without the conditions

--- src/badges/tests.py
+++ src/badges/tests.py
@@ -16,6 +16,11 @@ class BadgeTestCase(TestCase):
             password='password',
             date_joined=timezone.now() - timedelta(days=365),
         )
+        self.user_without_badges = User.objects.create_user(
+            username='without_badge',
+            password='password',
+            date_joined=timezone.now(),
+        )
 
     def test_star(self):
         model = Model.objects.create(user=self.user, name='cube')
@@ -24,6 +29,7 @@ class BadgeTestCase(TestCase):
             self.client.get('/models/' + model.name)
 
         self.assertTrue(Star.objects.filter(user=self.user))
+        self.assertFalse(Star.objects.filter(user=self.user_without_badges))
 
     def test_collector(self):
         self.client.login(username=self.user.username, password='password')
@@ -33,10 +39,12 @@ class BadgeTestCase(TestCase):
                 self.client.post('/models', data={'file': fd})
 
         self.assertTrue(Collector.objects.filter(user=self.user))
+        self.assertFalse(Collector.objects.filter(user=self.user_without_badges))
 
     def test_pioneer(self):
         self.client.login(username=self.user.username, password='password')
         self.assertTrue(Pioneer.objects.filter(user=self.user))
+        self.assertFalse(Pioneer.objects.filter(user=self.user_without_badges))
 
     def test_power_user(self):
         self.client.login(username=self.user.username, password='password')
@@ -44,3 +52,4 @@ class BadgeTestCase(TestCase):
                 self.client.post('/models', data={'file': fd})
 
         self.assertTrue(PowerUser.objects.filter(user=self.user))
+        self.assertFalse(PowerUser.objects.filter(user=self.user_without_badges))
